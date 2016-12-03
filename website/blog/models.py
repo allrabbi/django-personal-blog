81e5a2371271fc71ccc4adc9a5ff92bb3a4b663e
bittu072 <bittu.patel072@gmail.com>
2016-12-03 01:11:12 -0800

    added blogpost inside blog page--which contains title and date. aldo configured admin page

--- website/blog/models.py
+++ website/blog/models.py
@@ -1,13 +1,9 @@
-from __future__ import unicode_literals
-
 from django.db import models
-
+#this you need to defin like column_name=datatype, here title is column name and charfield is data type
 class Post(models.Model):
-    #this you need to defin like column_name=datatype, here title is column name and charfield is data type
-    title = models.CharField(max_length=140)
+    title = models.CharField(max_length = 140)
     body = models.TextField()
     date = models.DateTimeField()
 
-    def __unicode__(self):
-        #if you are using python3 then use __str__ instead of unicode
+    def __str__(self):
         return self.title
